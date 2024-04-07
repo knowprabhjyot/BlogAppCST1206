a0d32b4b365bc731484997a601764985038b9f68
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2024-04-06 18:11:27 -0700

    Finalized code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 4000;
+const PORT =  process.env.PORT || 4000;
 require('dotenv').config();
 const UserRoutes = require('./routes/User');
 
